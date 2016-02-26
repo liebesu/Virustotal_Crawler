f27305b48c5f2263d576e320566b86fe938130cc
liebesu <lichunjiang42@gmail.com>
2016-02-26 18:32:11 +0800

    update
    update

--- pagetest3.py
+++ pagetest3.py
@@ -6,6 +6,7 @@ login_data={'next':'/en/',
     'response_format': 'json',
     'username': 'polydata123',
     'password': 'polydata',
+    'VT_CSRF': '2deeb6fcc1e3acd678a2f0e2c096fb9d',
             }
 s=requests.session()
 a=s.post('https://www.virustotal.com/en/account/signin/',login_data)
